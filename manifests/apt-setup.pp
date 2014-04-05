c7af971fb534ab341ed576aedc555ce6e55531bc
Dan Morrison <dan@coders.co.nz>
2014-04-05 17:27:15 +1300

    small doc notes and a notice in apt setup

--- manifests/apt-setup.pp
+++ manifests/apt-setup.pp
@@ -1,17 +1,16 @@
-# If I wanted settings for apt, set them here.
+notice("Setting up apt-get to use a local proxy")
 
 # Using the module distributed by puppetlabs.
-
 class { 'apt':
-  always_apt_update    => true,
-  disable_keys         => undef,
+  # If I wanted settings for apt, set them here.
+  always_apt_update    => false,
+  #disable_keys         => undef,
   proxy_host           => '192.168.56.101',
   proxy_port           => '3142',
-  purge_sources_list   => false,
-  purge_sources_list_d => false,
-  purge_preferences_d  => false,
-  update_timeout       => undef
+  #purge_sources_list   => false,
+  #purge_sources_list_d => false,
+  #purge_preferences_d  => false,
+  #update_timeout       => undef
 }
-
 
 include "apt"
