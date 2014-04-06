28c965ef6639f9bba1b6fa4c89033bf2b5653634
Dan Morrison <dan@coders.co.nz>
2014-04-07 01:13:06 +1200

    Set up php using the basics : package installations and config settings - using augeas to edit the php.ini file

---
+++ manifests/php-setup.pp
@@ -0,0 +1,31 @@
+notice("Setting up PHP with preferred settings and extensions")
+
+class php-setup {
+  import "apt-setup"
+
+  notice("Checking PHP extensions")
+  package { [
+      "php5",
+      "php5-cli",
+      "php5-curl",
+      "php5-gd",
+      "php5-mysql"
+    ]:
+    ensure => present,
+    require => Exec["apt-get update"],
+  }
+
+  notice("Checking PHP settings")
+  # Use augeas for ini file managment. MUST have a new version of Puppet though!.
+  augeas { "Set PHP settings" :
+    context => "/files/etc/php5/fpm/php.ini/PHP",
+    changes => [
+      "set upload_max_filesize 16M",
+      "set post_max_size 16M",
+      #"set PHP/display_errors On",
+    ],
+  }
+
+}
+
+include php-setup
