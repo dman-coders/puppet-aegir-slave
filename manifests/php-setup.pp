aa85a4fb4327aa12163fa76d454410c8570cacac
Dan Morrison <dan@coders.co.nz>
2014-04-08 01:51:46 +1200

    pear for php - so drush and things will be happy

--- manifests/php-setup.pp
+++ manifests/php-setup.pp
@@ -3,20 +3,21 @@ notice("Setting up PHP with preferred settings and extensions")
 class php-setup {
   import "apt-setup"
 
-  notice("Checking PHP extensions")
+  notice(" - Checking PHP extensions")
   package { [
       "php5",
       "php5-cli",
       "php5-curl",
       "php5-gd",
-      "php5-mysql"
+      "php5-mysql",
+      "php-pear",
     ]:
     ensure => present,
     require => Exec["apt-get update"],
   }
 
-  notice("Checking PHP settings")
-  # Use augeas for ini file managment. MUST have a new version of Puppet though!.
+  notice(" - Checking PHP settings")
+  # Use augeas for ini file managment. MUST have a new version of Puppet though!
   augeas { "Set PHP settings" :
     context => "/files/etc/php5/fpm/php.ini/PHP",
     changes => [
