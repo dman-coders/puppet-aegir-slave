974636b4f8dddb1d0f994f889c0c3fc3c4a35c58
Dan Morrison <dan@coders.co.nz>
2014-04-08 01:53:40 +1200

    adding mysql basic server. The user account given to aegir still needs work though

---
+++ manifests/mysql-setup.pp
@@ -0,0 +1,10 @@
+notice("Setting up Mysql")
+
+# Using the module distributed by puppetlabs.
+# https://github.com/puppetlabs/puppetlabs-mysql#usage
+
+class { 'mysql::server':
+  #override_options => { 'mysqld' => { 'max_connections' => '1024' } }
+}
+include "mysql::server"
+
