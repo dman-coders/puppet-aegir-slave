814b87d5806efaf28a68d41c3df4b4d2e1e6c3c2
Dan Morrison <dan@coders.co.nz>
2014-05-14 23:57:03 +1200

    Aegir slave expects mysql port to be open

--- manifests/mysql-setup.pp
+++ manifests/mysql-setup.pp
@@ -2,8 +2,15 @@ notice("We will want Mysql")
 
 # Using the module distributed by puppetlabs.
 # https://github.com/puppetlabs/puppetlabs-mysql#usage
+# For Aegir to connect to this and push database, need the MYSQL port open.
+# Note - this makes it vital o start using a very good aegir DB pass!
 class  { 'mysql::server':
   #override_options => { 'mysqld' => { 'max_connections' => '1024' } }
+  override_options => {
+    'mysqld' => {
+      'bind_address' => '0.0.0.0'
+    }
+  }
 }
 
 include 'mysql::client'
