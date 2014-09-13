9f57760e9e3ae3470feae0e55ebdb4e6509443d4
Dan Morrison <dan@coders.co.nz>
2014-09-13 23:42:17 +1200

    Try to use a more forgiving pat-cache method. Following example from http://askubuntu.com/questions/53443/how-do-i-ignore-a-proxy-if-not-available to use my local apt-cache if available, but with a fall-through to normal repos if not

---
+++ files/setup-server.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Assume we are running as root
+
+export PROJECTNAME="aegir-slave"
+
+# This often complains on a new machine
+sudo locale-gen en_US.UTF-8
+sudo sh -c "echo 'LC_ALL=en_US.UTF-8\nLANG=en_US.UTF-8' >> /etc/environment"
+
+export HOSTNAME=$PROJECTNAME
+sudo sh -c "echo $HOSTNAME > /etc/hostname"
+sudo sh -c "echo '127.0.1.1 $HOSTNAME' >> /etc/hosts"
+
