ca6f327d21f96ba492337772373590d04c803883
Dan Morrison <dan@coders.co.nz>
2014-09-14 04:13:54 +1200

    see if we can rename the box

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -2,7 +2,7 @@
 # Start listing the sub-tasks
 # Try to keep any task-specific logic out of here,
 # just import or include the individual pps
-node default {
+node slave {
   require 'util-setup'
   require 'aegir-slave-setup'
 }
