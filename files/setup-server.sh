7a2670dcf5e8d6bcee5dbe5605c529eadb0780f4
Dan Morrison <dan@coders.co.nz>
2015-10-12 13:35:35 +1300

    Update setup-server.sh
    Looks like an extra step may be needed to finalize the locale issue. (ubuntu 14 at least)

--- files/setup-server.sh
+++ files/setup-server.sh
@@ -6,6 +6,7 @@ export PROJECTNAME="aegir-slave"
 # This often complains on a new machine
 sudo locale-gen en_US.UTF-8
 sudo sh -c "echo 'LC_ALL=en_US.UTF-8\nLANG=en_US.UTF-8' >> /etc/environment"
+sudo dpkg-reconfigure locales
 
 export HOSTNAME=$PROJECTNAME
 sudo sh -c "echo $HOSTNAME > /etc/hostname"
